d740194ea686f68f09f60de622565557e5417b37
Harshvardhan Chauhan <112753472+harshvardhangit@users.noreply.github.com>
2023-04-17 10:21:35 +0530

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,1 +1,39 @@
+                                              Arrays Problems
+                                             -----------------
 
+
+1-> print Array
+
+
+public class array_print_using_loop {
+
+	public static void main(String[] args) {
+		int[] a= {11,22,33,44,55,66,77};
+		
+		for(int i=0;i<a.length;i++)
+			
+		{
+			System.out.println("array of index "+i+" element "+a[i]);
+		}
+
+	}
+
+}
+
+output -> ---------------------------------
+  
+  array of index 0 element 11
+  array of index 1 element 22
+  array of index 2 element 33
+  array of index 3 element 44
+  array of index 4 element 55
+  array of index 5 element 66
+  array of index 6 element 77
+  
+--------------------------------------------
+  
+====================================================//============================================================================================================
+  
+  
+  
+2-> 
